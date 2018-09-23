d6ec263ca822737eb4824fc2bc9be54bc0551338
Krishna Kumar T <krishna.thokala2010@gmail.com>
2018-09-23 19:46:11 +0530

    [Krishna] First cut casync, can chunk a file into chunks based on rolling buzhash algo

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+extern crate hash_roll;
+
+use std::fs::File;
+use std::io::Error;
+use std::io::Read;
+use std::io::Write;
+
+use hash_roll::buzhash::BuzHash;
+use hash_roll::buzhash::BuzHashBuf;
+
+use std::collections::VecDeque;
+
+struct Chunker {
+    chunk_count: u64,
+}
+
+impl Chunker{
+    fn get_new_chunk_file_name(&mut self) -> std::string::String {
+        self.chunk_count += 1;
+        let mut file_no = (self.chunk_count).to_string();
+        file_no.push_str(".cnk");
+        file_no
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let file_to_read = get_file_to_read();
+    //let mut file_to_write = get_file_to_write();
+
+    //let mut bytes = [0;1];
+    //for byte in file_to_read.bytes(){
+    //    bytes[0] = byte.unwrap();
+    //    file_to_write.write(&bytes);
+    //}
+
+    let mut b = BuzHashBuf::from(BuzHash::with_capacity(7));
+    let h = {
+        let mut m = b.clone();
+        m.push(&[0,0,0,0,0,0,0]);
+        m.hash()
+    };
+
+    let mut chunker = Chunker{chunk_count: 0};
+
+    println!("Match found at {:?}",process_chunks(&mut b,h,file_to_read,&mut chunker));
+}
+
+fn get_file_to_read() -> File {
+    File::open("input.txt").unwrap()
+}
+
+fn get_file_to_write(file_name: &str) -> File {
+    File::create(file_name).unwrap()
+}
+
+fn create_chunk_file(chunker: &mut Chunker, data: &VecDeque<u8>) {
+    let mut file_to_write = get_file_to_write(&chunker.get_new_chunk_file_name());
+    file_to_write.write_all(data.as_slices().0);
+}
+
+fn process_chunks(b: &mut BuzHashBuf, other_hash: u8, file: File, chunker: &mut Chunker) -> usize {
+    let mut chunk_buf = VecDeque::new();
+    for (i, v) in file.bytes().enumerate() {
+        let each_byte = v.unwrap();
+        chunk_buf.push_back(each_byte.clone());
+        b.push_byte(each_byte);
+        if b.hash() == other_hash {
+            //println!("length of chunk, {}",chunk_buf.len());
+            create_chunk_file(chunker, &chunk_buf);
+            chunk_buf = VecDeque::new();
+            //return i+1;
+        }
+    }
+    0
+}
+
